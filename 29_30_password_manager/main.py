1448f071f2894279e4a6f1f9e226d1d12eeb4c20
Lukas Dempwolff <dempwolff@pm.me>
2025-10-29 11:22:17 +0100

    studying CW44.3

--- 29_30_password_manager/main.py
+++ 29_30_password_manager/main.py
@@ -36,10 +36,41 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo("Oops", "Please don't leave any fields empty")
     else:
-         with open("data.json", "w") as data_file:
-            json.dump(new_data, data_file, indent = 4)
+        try:
+            with open("data.json", "r") as data_file:
+                # Read old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                # Saving new data
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Update old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent = 4)
+        finally:
             website_entry1_1.delete(0, END)
             password_entry1_3.delete(0, END)
+# ---------------------------- SEARCH ------------------------------- #
+def find_password():
+    website = website_entry1_1.get()
+    if len(website) == 0:
+        messagebox.showinfo("Oops", "Please don't leave any fields empty")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                query = data[website]
+        except FileNotFoundError:
+            messagebox.showinfo("Oops", "No Data File Found")
+        except KeyError:
+            messagebox.showinfo("Oops", "No details for the website exists")
+        else:
+            messagebox.showinfo(title=website, message=f"Email: {query['email']}\nPassword: {query['password']}")
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -61,8 +92,8 @@ canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
-website_entry1_1 = Entry(width=52)
-website_entry1_1.grid(column=1, row=1, columnspan=2)
+website_entry1_1 = Entry(width=33)
+website_entry1_1.grid(column=1, row=1)
 website_entry1_1.focus()
 
 mail_entry1_2 = Entry(width=52)
@@ -76,6 +107,10 @@ add_button1_4 = Button(text="Add", width=44, command=save)
 add_button1_4.grid(column=1, row=4, columnspan=2)
 
 #---column2
+button2_1 = Button(text="Search", width= 15,command=find_password)
+button2_1.grid(column=2, row=1)
+
+
 button2_3 = Button(text="Generate Password", command=generate_password)
 button2_3.grid(column=2, row=3)
 # ---------------------------- END ------------------------------- #
