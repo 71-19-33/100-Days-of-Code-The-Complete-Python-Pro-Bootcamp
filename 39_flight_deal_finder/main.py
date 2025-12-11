b15cdabad3c8af8930828c7dfdfc60769b9b5607
Lukas Dempwolff <dempwolff@pm.me>
2025-12-11 15:21:10 +0100

    studying CW50.4_2

--- 39_flight_deal_finder/main.py
+++ 39_flight_deal_finder/main.py
@@ -8,8 +8,10 @@ from flight_search import FlightSearch
 sheet = DataManager()
 flight_data = FlightSearch()
 
+#get cities from sheet
 cities = [entry["city"] for entry in sheet.get_data()["prices"]]
 #['Paris', 'Frankfurt', 'Tokyo', 'Hong Kong', 'Istanbul', 'Kuala Lumpur', 'New York', 'San Francisco', 'Dublin']
+#get iata city codes for cities
 iata_city_codes = flight_data.get_iata_city_codes(cities)
 #['PAR', 'FRA', 'TYO', 'HKG', 'IST', 'KUL', 'NYC', 'SFO', 'DBN']
 
