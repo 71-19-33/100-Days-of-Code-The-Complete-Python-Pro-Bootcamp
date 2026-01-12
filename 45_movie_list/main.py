19999e9715fe5a9c446308c2cd48757259d80ab1
Lukas Dempwolff <dempwolff@pm.me>
2026-01-12 16:23:20 +0100

    Studying CW03.1

---
+++ 45_movie_list/main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+with open(r"45_movie_list\website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.get("href"))
