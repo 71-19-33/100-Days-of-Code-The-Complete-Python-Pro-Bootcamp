d1f99571f414293670f7f4a8437de02a7a57325e
Lukas Dempwolff <dempwolff@pm.me>
2026-02-16 16:48:29 +0100

    Studying CW08.1

---
+++ 48_selenium_webdriver_browser_game/interaction.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+# article_count = driver.find_element(By.XPATH, '//*[@id="articlecount"]/ul/li[2]/a[1]')
+# print(article_count.text)
+# article_count.click()
+
+# all_portals = driver.find_element(By.LINK_TEXT, "Content portals")
+# all_portals.click()
+
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+
+# driver.quit()
--- 48_selenium_webdriver_browser_game/main.py
+++ 48_selenium_webdriver_browser_game/main.py
@@ -2,10 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
-driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
 
-price_whole= driver.find_element(By.CLASS_NAME, "a-price-whole")
-price_fraction = driver.find_element(By.CLASS_NAME, "a-price-fraction")
-print(f"Price: {price_whole.text}.{price_fraction.text} €")
+#-----Amazon price scraping:
+# driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
+
+# price_whole= driver.find_element(By.CLASS_NAME, "a-price-whole")
+# price_fraction = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+# print(f"Price: {price_whole.text}.{price_fraction.text} €")
+
+#-----Get the event name and date from the website:
+driver.get("https://www.python.org/")
+
+event_dates = driver.find_elements(By.CSS_SELECTOR, ".event-widget .menu time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget .menu a")
+
+event_schedule = {
+    i: {"time": date.text, "name": name.text} for i, (date, name) in enumerate(zip(event_dates, event_names))
+}
+
+print(event_schedule)
 
 driver.quit()
